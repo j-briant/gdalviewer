33b494e944ace299ac99726d5a609a6824739bb3
j-briant <j.briant@protonmail.com>
2024-05-20 20:32:32 +0200

    hum

--- src/app.rs
+++ src/app.rs
@@ -1,13 +1,30 @@
 use eframe::egui;
-use egui::Align;
-use egui_file_dialog::FileDialog;
-use gdal::Dataset;
 use std::path::PathBuf;
 
-#[derive(Default)]
 pub struct ViewerPage {
     text: String,
     is_open: bool,
+}
+
+impl Default for ViewerPage {
+    fn default() -> Self {
+        Self {
+            text: String::from("viewer"),
+            is_open: false,
+        }
+    }
+}
+
+impl From<&PathBuf> for ViewerPage {
+    fn from(value: &PathBuf) -> Self {
+        match value.to_str() {
+            Some(s) => Self {
+                text: String::from(s),
+                is_open: true,
+            },
+            None => Default::default(),
+        }
+    }
 }
 
 impl ViewerPage {
@@ -16,15 +33,11 @@ impl ViewerPage {
             text: s,
             is_open: true,
         }
-        // This is also where you can customize the look and feel of egui using
-        // `cc.egui_ctx.set_visuals` and `cc.egui_ctx.set_fonts`.
     }
 
-    pub fn show(&mut self, ctx: &egui::Context, open: &bool) {
-        let viewport_id: egui::ViewportId = egui::ViewportId::from_hash_of(format!("edit test"));
-        let viewport_builder = egui::ViewportBuilder::default()
-            .with_title(&self.text)
-            .with_close_button(true);
+    pub fn show(&mut self, ctx: &egui::Context) {
+        let viewport_id: egui::ViewportId = egui::ViewportId::from_hash_of("edit test");
+        let viewport_builder = egui::ViewportBuilder::default().with_title(&self.text);
         let viewport_cb = |ctx: &egui::Context, _| {
             egui::CentralPanel::default().show(ctx, |ui| {
                 ui.label("Name:");
@@ -32,47 +45,16 @@ impl ViewerPage {
             });
         };
         ctx.show_viewport_immediate(viewport_id, viewport_builder, viewport_cb);
-    }
-}
-pub struct HomePage {
-    picked_path: Option<String>,
-    show_window: bool,
-}
-
-impl HomePage {
-    fn new() -> Self {
-        Self {
-            picked_path: None,
-            show_window: true,
-        }
-    }
-
-    fn show(&mut self, ctx: &egui::Context, open: &mut bool) {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.label("Drag-and-drop files onto the window!");
-            if ui.button("Open file…").clicked() {
-                if let Some(path) = rfd::FileDialog::new().pick_file() {
-                    self.picked_path = Some(path.display().to_string());
-                }
-            }
-        });
-    }
-}
-
-impl Default for HomePage {
-    fn default() -> Self {
-        HomePage {
-            picked_path: None,
-            show_window: true,
+        if ctx.input(|i| i.viewport().close_requested()) {
+            // Tell parent viewport that we should not show next frame:
+            self.is_open = false;
         }
     }
 }
 
 #[derive(Default)]
 pub struct ViewerApp {
-    homepage: HomePage,
-    viewer: ViewerPage,
-    datasets: Vec<Dataset>,
+    viewer: Vec<ViewerPage>,
 }
 
 impl ViewerApp {
@@ -86,13 +68,11 @@ impl eframe::App for ViewerApp {
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.label("Drag-and-drop files onto the window!");
             if ui.button("Open file…").clicked() {
-                if let Some(path) = rfd::FileDialog::new().pick_file() {
-                    self.viewer = ViewerPage::new("text".into());
+                if let Some(path) = rfd::FileDialog::new().pick_files() {
+                    self.viewer = path.iter().map(ViewerPage::from).collect();
                 }
             }
-            self.viewer.show(ctx, &self.viewer.is_open);
         });
-
         /* if let Some(picked_path) = &self.picked_path {
             ui.horizontal(|ui| {
                 ui.label("Picked file:");
